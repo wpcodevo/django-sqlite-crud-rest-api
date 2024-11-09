1bd26fd4b45414961e6c66b90d6ad8bed89c8a63
Edem <contact@codevoweb.com>
2024-11-09 20:27:24 +0000

    updated

--- feedback_api/models.py
+++ feedback_api/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 class FeedbackModel(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=255, unique=True)
-    email = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
     feedback = models.CharField(max_length=255, unique=True)
-    status = models.CharField(max_length=255, unique=True)
+    status = models.CharField(max_length=255)
     rating = models.FloatField()
     createdAt = models.DateTimeField(auto_now_add=True)
     updatedAt = models.DateTimeField(auto_now=True)
